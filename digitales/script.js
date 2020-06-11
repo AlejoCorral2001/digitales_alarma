4fc60e67576fed84f53e7a8865e404cf15d132d1
AlejoCorral2001 <62577197+AlejoCorral2001@users.noreply.github.com>
2020-06-10 23:52:57 -0300

    Add files via upload

---
+++ digitales/script.js
@@ -0,0 +1,125 @@
+
+// click blinking alarmLight alarmSound stopButton
+
+
+
+let ledB=0;
+let timer = '';
+blinking = function(x){
+if(x==1){    
+    //input = document.getElementById();
+    if(timer==''){
+        timer = setInterval(()=>{
+            console.log('blink')
+            let led = document.getElementById('led')
+            led.setAttribute('style','background-color:'+(ledB==0?' rgb(104, 241, 63)':'green'));
+            ledB=!ledB
+        },250)
+    }
+}else{
+    clearInterval(timer)
+    timer=''
+    let led = document.getElementById('led')
+            led.setAttribute('style','background-color:rgb(104, 241, 63)');
+            ledB=0;
+}
+}
+
+let timer2 = '';
+let backS=0
+alarmLight = function(x){
+    if(x==1){    
+        //input = document.getElementById();
+        if(timer2==''){
+            timer2 = setInterval(()=>{
+               
+                let back = document.getElementById('back')
+                back.setAttribute('style','background-color:'+(backS==0?'rgb(233, 89, 89)':'white'));
+                backS=!backS
+            },600)
+        }
+    }else{
+        clearInterval(timer2)
+        timer2=''
+        let back = document.getElementById('back')
+                back.setAttribute('style','background-color:white');
+                backS=0;
+    }
+
+}
+alarmSound = function(x){
+    if (x==1) {
+        document.getElementById('led2').setAttribute('style','background-color: red')
+    }else{
+        document.getElementById('led2').setAttribute('style','background-color: rgb(49, 32, 21)')
+    }
+
+}
+
+
+
+
+
+
+step1 = function(x){
+    if (step==1) {
+        if(x==2){
+            test=1;
+            blinking(1);
+            console.log('comienza en modo test')
+        }else{
+            blinking(1)
+            console.log('comienza la espera a activacion de sensor')
+            setTimeout(()=>{
+                blinking(0)
+            },2000)
+           
+        }
+
+        step=2;    
+    }   
+}
+
+alarmButton = function(){
+    if(step==2){
+        if(test==1){    //modo test
+            blinking(0)
+            alarmLight(1)
+        
+        }else{  //modo normal
+            alarmLight(1)
+            alarmSound(1)
+        }
+        console.log('se activo la sirena')
+        step=3;
+
+    }
+}
+
+alarmStopButton = function(){
+    if(step==3){
+        alarmLight(0)
+        alarmSound(0)
+        step=1;
+        console.log('se desactivo la alarma')
+    }
+}
+
+let contador = 0
+let step=1;
+let test=0;
+let input = document.getElementById('button1');
+input.addEventListener('click',()=>step1(1));
+input = document.getElementById('button2');
+input.addEventListener('click',()=>step1(2));
+input = document.getElementById('button3');
+input.addEventListener('click',alarmButton)
+input = document.getElementById('button4');
+input.addEventListener('click',alarmStopButton)
+
+//inicio
+
+
+
+
+
